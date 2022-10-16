14429a208e46084c91fcae300f6fe6e22f6c4b5c
sarath baiju <sarath.bu@frontiersin.net>
2022-10-16 22:26:03 +0530

    Intial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private readonly prismaService: PrismaService){
+
+  }
+
+  async create(createUserDto: CreateUserDto) {
+   await this.prismaService.user.create({
+      data: {
+        name: createUserDto.name
+      }
+    })
+  }
+
+  async findAll() {
+    return await this.prismaService.user.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
